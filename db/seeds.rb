2f7704db7499c07f69674fb049f62c84960fb9c6
billyott <willrichott@gmail.com>
2020-10-19 17:22:23 -0400

    Created seeds for User, Game, System, GameSystem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,44 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-url = "https://api.rawg.io/api/games?page=1"
-response = RestClient.get(url)
-data = JSON.parse(response)
+# #system seeds
 
+# url = "https://api.rawg.io/api/platforms"
+# response = RestClient.get(url)
+# data = JSON.parse(response)
 
-data["results"].each do |game|
-    name = game["name"]
-    rating = game["rating"]
-    esrb_rating = game["everyone"]
-    rating_count = game["reviews_count"]
-    genre = game["genres"][0]["name"]
-   
-    Game.create()
-end
+# data["results"].each do |system|
+#     id = system["id"]
+#     name = system["name"]
+#     System.create(name: name, id: id)
+# end
+
+# # game seeds
+# Game.destroy_all
+# GameSystem.destroy_all
+
+# url = "https://api.rawg.io/api/games?page=1"
+# response = RestClient.get(url)
+# data = JSON.parse(response)
+
+# data["results"][0..9].each do |game|
+#     name = game["name"]
+#     rating = game["rating"]
+#     esrb_rating = "everyone"
+#     rating_count = game["reviews_count"]
+#     genre = game["genres"][0]["name"]
+#     platform_ids = game["platforms"].map{|platform| platform["platform"]["id"]}
+#     game = Game.create(name: name, rating: rating, esrb_rating: esrb_rating, rating_count: rating_count, genre: genre)
+#     platform_ids.each do |platform_id|
+#         GameSystem.create(game_id: game.id, system_id: platform_id)
+#     end
+# end
+
+User.destroy_all
+
+#user seeds
+sisan = User.create(name: "Sisan", age: 21, interest: "Horror and Adventure games")
+reid = User.create(name: "Reid", age: 26, interest: "Fantasy and Strategy games")
+bill = User.create(name: "Bill", age: 31, interest: "pokemon")
+greg = User.create(name: "Greg", age: 31, interest: "codin'")
+caryn = User.create(name: "Caryn", age: 26, interest: "cats")
