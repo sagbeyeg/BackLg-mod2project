a7f74532d7a4256dcdf79aec49460451bb2e613b
billyott <willrichott@gmail.com>
2020-10-19 14:30:27 -0400

    Working to add Game seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+url = "https://api.rawg.io/api/games?page=1"
+response = RestClient.get(url)
+data = JSON.parse(response)
+
+
+data["results"].each do |game|
+    name = game["name"]
+    rating = game["rating"]
+    esrb_rating = game["everyone"]
+    rating_count = game["reviews_count"]
+    genre = game["genres"][0]["name"]
+   
+    Game.create()
+end
