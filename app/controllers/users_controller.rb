e9d0c7853afab57443ec2588f9c390003e6c5252
billyott <willrichott@gmail.com>
2020-10-20 12:13:44 -0400

    Added User views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @users = User.all
+    end
+
+    def show
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            redirect_to user_path(@user)
+        else
+            flash[:my_errors] = @user.errors.full_messages
+            redirect_to new_banana_path
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @user.update(user_params)
+        redirect_to user_path(@user)
+    end
+
+    def destroy
+        @user.destroy
+        redirect_to users_path
+    end
+
+    private
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :age, :interest)
+    end
 end
