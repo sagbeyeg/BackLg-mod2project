cd42c76520493db2d531000aac1d3c39d553521d
sagbeyeg <agbeyegbesisan@gmail.com>
2020-10-20 12:27:12 -0400

    Added New and Edit views for reviews, need to work on setting the current user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,42 @@
 class ReviewsController < ApplicationController
+  before_action :find_review, only: [:edit, :update]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user if current_user
+    if @review.valid
+      # flash[:success] = "Your review for #{@review.game.name} has been created"
+      # redirect_to 
+    else
+      flash[:error] = "Please try again, information is incomplete"
+      redirect_to 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @review.user_id = current_user if current_user
+    if @review.update(review_params)
+      # redirect_to 
+    else 
+      flash[:error] = "Please try again, something went wrong"
+      redirect_to edit_review_path(@review)
+    end
+  end
+
+  private
+
+  def find_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:user_id, :game_id, :rating, :description, :title)
+  end
 end
