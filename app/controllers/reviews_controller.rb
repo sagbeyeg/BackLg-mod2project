77cb4c840577475196622cbefbb73e0f38b77117
jrwatson94 <j.watsonreid@gmail.com>
2020-10-22 10:55:57 -0400

    fixing bugs

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -33,6 +33,12 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to user_path(@review.user)
+  end
+
   private
 
   def find_review
